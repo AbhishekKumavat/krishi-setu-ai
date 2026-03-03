4f869cfa85431a9a30374b31c6963f82e6d6012e
AbhishekKumavat <abhishekkumavat443@gmail.com>
2026-03-03 14:41:54 +0530

    one last

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://yzmwwxllkzzjodczfkvh.supabase.co'; // Fallback to avoid build crash
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'public-anon-key';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://jlldtawccoagkbweatgn.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsbGR0YXdjY29hZ2tid2VhdGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MTUzNzQsImV4cCI6MjA4ODA5MTM3NH0.cKe6HJdz9NG1t7sR1FHPnVWKAyE1qf1Duk1hlEyKO_0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
